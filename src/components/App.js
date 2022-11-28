f41df1126b34e331995731a231dfab6c462397a5
Blanca Ubis <ubis.blanca@gmail.com>
2022-11-28 09:12:38 +0100

    Make npm githubpages work

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
 import '../styles/App.scss';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h1>Adalabers</h1>
+      <table className="table">
+        {/* <!-- Fila de cabecera --> */}
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        {/* <!-- Fin fila de cabecera --> */}
+        <tbody>
+          {/* <!-- Primera fila --> */}
+          <tr>
+            <td>MariCarmen</td>
+            <td>Yanelis</td>
+            <td>Python</td>
+          </tr>
+          {/* <!-- Segunda fila --> */}
+          <tr>
+            <td>Amparo</td>
+            <td>Dayana</td>
+            <td>IA</td>
+          </tr>
+          {/* <!-- Tercera fila --> */}
+          <tr>
+            <td>Escandia</td>
+            <td>Iván</td>
+            <td>3D graphics</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
