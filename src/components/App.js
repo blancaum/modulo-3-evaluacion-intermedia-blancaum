1b50337b2bcd0d13401e3b7b60d886963844dcd5
Blanca Ubis <ubis.blanca@gmail.com>
2022-11-28 09:29:36 +0100

    Add CSS classes

--- src/components/App.js
+++ src/components/App.js
@@ -19,38 +19,42 @@ function App() {
 
   return (
     <div>
-      <h1>Adalabers</h1>
-      <table className="table">
-        {/* <!-- Fila de cabecera --> */}
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Tutora</th>
-            <th>Especialidad</th>
-          </tr>
-        </thead>
-        {/* <!-- Fin fila de cabecera --> */}
-        <tbody>
-          {/* <!-- Primera fila --> */}
-          <tr>
-            <td>MariCarmen</td>
-            <td>Yanelis</td>
-            <td>Python</td>
-          </tr>
-          {/* <!-- Segunda fila --> */}
-          <tr>
-            <td>Amparo</td>
-            <td>Dayana</td>
-            <td>IA</td>
-          </tr>
-          {/* <!-- Tercera fila --> */}
-          <tr>
-            <td>Escandia</td>
-            <td>Iván</td>
-            <td>3D graphics</td>
-          </tr>
-        </tbody>
-      </table>
+      <header>
+        <h1 className="title">Adalabers</h1>
+      </header>
+      <main>
+        <table className="table">
+          {/* <!-- Fila de cabecera --> */}
+          <thead className="table__thead">
+            <tr>
+              <th>Nombre</th>
+              <th>Tutora</th>
+              <th>Especialidad</th>
+            </tr>
+          </thead>
+          {/* <!-- Fin fila de cabecera --> */}
+          <tbody className="table__tbody">
+            {/* <!-- Primera fila --> */}
+            <tr>
+              <td>MariCarmen</td>
+              <td>Yanelis</td>
+              <td>Python</td>
+            </tr>
+            {/* <!-- Segunda fila --> */}
+            <tr>
+              <td>Amparo</td>
+              <td>Dayana</td>
+              <td>IA</td>
+            </tr>
+            {/* <!-- Tercera fila --> */}
+            <tr>
+              <td>Escandia</td>
+              <td>Iván</td>
+              <td>3D graphics</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
